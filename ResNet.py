73b8bc15eac9a0fb0cbf6eb55eeb062ad1f8642a
Kyudan Jung (정규단) <116885375+KyuDan1@users.noreply.github.com>
2024-03-31 12:29:05 +0900

    Delete ResNet.py

--- ResNet.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch.nn as nn
-
-class BasicBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3):
-        super(BasicBlock, self).__init__()
-
-        # 합성곱층 정의
